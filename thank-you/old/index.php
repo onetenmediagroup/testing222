886e6068a23833a53b5f4d91682735bbde776fb9
Albert Lu <albert@onetenmediagroup.com>
2018-09-03 09:15:16 +0000

    thank you folder

---
+++ thank-you/old/index.php
@@ -0,0 +1,13 @@
+<?php header('P3P:policyref="https://googleads.g.doubleclick.net/pagead/gcn_p3p_.xml", CP="CURa ADMa DEVa TAIo PSAo PSDo OUR IND UNI PUR INT DEM STA PRE COM NAV OTC NOI DSP COR"'); ?> <!DOCTYPE html> 
+<html> 
+<head>
+ <meta name="referrer" content="no-referrer" /> 
+</head> 
+<body> 
+
+<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1900800450189453&ev=PageView"/>
+<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1900800450189453&ev=Purchase&cd[currency]=USD&cd[value]=0.00"/>
+
+<title>Thank You!</title> </body> </html>
+
+
