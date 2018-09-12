79fb64b45d1a85b0b1a13fae5522975f8794199b
Albert Lu <albert@onetenmediagroup.com>
2018-09-12 06:53:15 +0000

    12-09-18-0653

---
+++ thank-you/old/pur.php
@@ -0,0 +1,14 @@
+<?php header('P3P:policyref="https://googleads.g.doubleclick.net/pagead/gcn_p3p_.xml", CP="CURa ADMa DEVa TAIo PSAo PSDo OUR IND UNI PUR INT DEM STA PRE COM NAV OTC NOI DSP COR"'); ?> <!DOCTYPE html> 
+<html> <head> 
+<meta name="referrer" content="no-referrer" /> 
+</head> 
+
+<body> 
+	<img height="1" width="1" style="display:none" src="//www.facebook.com/tr?id=1900800450189453&ev=PageView&noscript=1" /> 
+	<img height="1" width="1" style="display:none" src="//www.facebook.com/tr?id=1900800450189453&ev=Purchase&noscript=1" /> 
+
+	<title>Thank You!</title> 
+	<h2>ATC</h2>
+</body> 
+
+</html>
